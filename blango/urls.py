f9a511a72081b384ba013bd01c8d99d285703a86
Osama Bin Azam <osamabinazam@gmail.com>
2023-06-16 18:57:15 +0500

    Post Detail Templates

--- blango/urls.py
+++ blango/urls.py
@@ -6,4 +6,5 @@ import blog.views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", blog.views.index, name="home"),
+    path("post/<slug>/", blog.views.post_detail, name = 'blog-post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.utils import timezone
 from blog.models import Post
 # Create your views here.
+
+# Home Page
 def index (request):
   posts = Post.objects.filter(published_at__lte=timezone.now())
   print(posts)
   context = {"posts" : posts}
   return render(request, "blog/index.html", context=context)
+
+
+# Post Detail
+
+def post_detail (request, slug):
+  post = get_object_or_404(Post, slug=slug)
+  context = {'post': post}
+  return render(request, 'blog/post-detail.html', context=context )
+ 

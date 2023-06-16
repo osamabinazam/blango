9e9bce0d606dd28c2bf1181d459afbf00e562d3d
Osama Bin Azam <osamabinazam@gmail.com>
2023-06-16 20:26:17 +0500

    Simple Django custom tags

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -32,3 +32,44 @@ def author_details(author, user=None):
 
 
 
+# Define Custom Row Template tag
+@register.simple_tag
+def row (extra_classes=""):
+  return format_html('<div class= "row {}" >', extra_classes)
+@register.simple_tag
+def endrow():
+  return format_html('</div>')
+
+
+# Define Custom Col Template tag
+@register.simple_tag
+def col(extra_classes=""):
+  return format_html(' <div class= "{}" >', extra_classes)
+@ register.simple_tag
+def endcol():
+  return format_html("</div>")
+
+
+# Just for practice purpose
+
+# Define Custom template tag for author detail
+# @register.simple_tag(takes_context=True)
+# def author_details_tag(context):
+#   request = context["request"]
+#   current_user =  request.user
+#   post = context["post"]
+#   author = post.author
+#   if author == current_user:
+#     return format_html("<strong>me</strong>")
+#   if author.first_name and author.last_name:
+#     name = f'{author.first_name} { author.last_name}'
+#   else : 
+#     name = f'{author.username}'
+#   if author.email:
+#     prefix = format_html('<a href= "mailto: {}">', author.email)
+#     suffix = format_html('</a>')
+#   else:
+#     prefix =""
+#     suffix = ""
+  
+#   return format_html('{} {} {}', prefix, name, suffix)

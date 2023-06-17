011b904ba45775d7d2b3e3b4aa856c1183424681
Osama Bin Azam <osamabinazam@gmail.com>
2023-06-17 20:05:47 +0500

    Django form

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 from blog.models import Post
+from blog.forms import CommentForm
 # Create your views here.
 
 # Home Page
@@ -15,6 +16,21 @@ def index (request):
 
 def post_detail (request, slug):
   post = get_object_or_404(Post, slug=slug)
-  context = {'post': post}
+  if request.user.is_active:
+    if request.method =='POST':
+      comment_form = CommentForm(request.POST)
+      if comment_form.is_valid():
+        comment = comment_form.save(commit=False)
+        comment.content_object=post
+        comment.creator=request.user
+        comment.save()
+        return redirect(request.path_info)
+    else :
+      comment_form =  CommentForm()
+  else:
+    comment_form = None
+  
+  
+  context = {'post': post, 'comment_form': comment_form}
   return render(request, 'blog/post-detail.html', context=context )
  
